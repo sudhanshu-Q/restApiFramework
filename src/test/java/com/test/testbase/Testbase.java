dca2d0b5df6ee753d132ced4eaec3720858b9223
sudhanshu-Q <sudhanshu.pat.kumar@gmail.com>
2024-12-17 14:50:04 +0530

    change for testbase

---
+++ src/test/java/com/test/testbase/Testbase.java
@@ -0,0 +1,36 @@
+package com.test.testbase;
+
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+
+public class Testbase {
+
+
+    public static Response response;
+
+    public HashMap<String, String> getHeaders_Common() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Connection", "keep-alive");
+        return map;
+    }
+
+    public Response response_GET(String url, HashMap<String, String> setHeader) {
+        response = given().headers(setHeader).baseUri(url).
+                when().get("").
+                then().extract().response();
+        response.then().log().all();
+        return response;
+    }
+
+    public Response response_GET_ID(String url, HashMap<String, String> setHeader, int id) {
+        response = given().headers(setHeader).baseUri(url).
+                when().get("").
+                then().extract().response();
+        response.then().log().all();
+        return response;
+    }
+}
+
